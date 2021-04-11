f38c860f04baec7e36e75f94ddec3c304c08964d
Conrad Schneggenburger <conrad.schnegg@gmail.com>
2021-04-11 13:19:19 -0400

    Dev (#4)
    * Add recording scripts
    
    * Update source to latest
    
    * Modify record script to include redo attempts
    
    * Bug fixes and trap function added
    
    * Modify generate script to output 10 files
    
    * Fix bug on timeout command with Ctrl+C
    
    * Reduce timeout to 90 seconds
    
    * Add more voice variants
    
    * Reduce timeout to 60 seconds
    
    * Remove unreliable voices from default configuration

--- generate.sh
+++ generate.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-voices=(cmu_us_aew cmu_us_ahw cmu_us_aup cmu_us_awb cmu_us_axb cmu_us_bdl cmu_us_clb cmu_us_eey cmu_us_fem cmu_us_gka cmu_us_jmk cmu_us_ksp cmu_us_ljm cmu_us_rms cmu_us_rxr cmu_us_slt)
+#voices=(cmu_us_aew cmu_us_ahw cmu_us_aup cmu_us_awb cmu_us_axb cmu_us_bdl cmu_us_clb cmu_us_eey cmu_us_fem cmu_us_gka cmu_us_jmk cmu_us_ksp cmu_us_ljm cmu_us_rms cmu_us_rxr cmu_us_slt)
+voices=(cmu_us_aew cmu_us_ahw cmu_us_awb cmu_us_bdl cmu_us_clb cmu_us_fem cmu_us_gka cmu_us_jmk cmu_us_ksp cmu_us_ljm cmu_us_rms cmu_us_rxr cmu_us_slt)
+#voices=(cmu_us_awb)
 
-# Prompt for directory paths
-while read -p "Enter the dataset directory [dataset]: " dataset_dir && dataset_dir=${dataset_dir:-"dataset"} && [ ! -d "$dataset_dir" ]; do
+# Prompt for data directory paths
+while read -p "Enter the dataset directory [current_dataset]: " dataset_dir && dataset_dir=${dataset_dir:-current_dataset} && [ ! -d $dataset_dir ]; do
   echo "Directory doesn't exist"
-done; echo -e "Using directory \"$dataset_dir\"\n"
-while read -p "Enter the command directory [$dataset_dir/commands]: " command_dir && command_dir=${command_dir:-"$dataset_dir/commands"} && [ ! -d "$command_dir" ]; do
+done; echo -e "Using directory $dataset_dir\n"
+while read -p "Enter the command directory [$dataset_dir/commands]: " command_dir && command_dir=${command_dir:-$dataset_dir/commands} && [ ! -d $command_dir ]; do
   echo "Directory doesn't exist"
-done; echo -e "Using directory \"$command_dir\"\n"
+done; echo -e "Using directory $command_dir\n"
 
 # Adjust the speed: flite -s duration_stretch=1.5
 # Adjust the average pitch: flite -s int_f0_target_mean=145.0
@@ -18,16 +20,35 @@ for voice in ${voices[@]}; do
   for command_subdir in $command_dir/*; do
     command=$(echo $command_subdir | rev | cut -f 1 -d "/" | rev | sed 's/_/ /g')
 
-    find $command_subdir -type f -name ${voice}_in001.wav -exec rm {} \;
-    echo "Generating $command_subdir/${voice}_in001.wav"
-    flite -voice flitevox/$voice.flitevox -o $command_subdir/${voice}_in001.wav -t "$command"
+    set -x
 
-    find $command_subdir -type f -name ${voice}_in002.wav -exec rm {} \;
-    echo "Generating $command_subdir/${voice}_in002.wav"
-    flite -voice flitevox/$voice.flitevox -o $command_subdir/${voice}_in002.wav -s "duration_stretch=1.25" -t "$command"
+    find $command_subdir -type f -name ${voice}_in001.wav -exec rm -f {} \; # Clears ONLY the file(s) corresponding to the selected voice
+    flite -voice voices/$voice.flitevox -o $command_subdir/${voice}_in001.wav -t "$command" -s "duration_stretch=1.00"
 
-    find $command_subdir -type f -name ${voice}_in003.wav -exec rm {} \;
-    echo "Generating $command_subdir/${voice}_in003.wav"
-    flite -voice flitevox/$voice.flitevox -o $command_subdir/${voice}_in003.wav -s "duration_stretch=1.50" -t "$command"
+    find $command_subdir -type f -name ${voice}_in002.wav -exec rm -f {} \; # Clears ONLY the file(s) corresponding to the selected voice
+    flite -voice voices/$voice.flitevox -o $command_subdir/${voice}_in002.wav -t "$command" -s "duration_stretch=1.25"
+
+    find $command_subdir -type f -name ${voice}_in003.wav -exec rm -f {} \; # Clears ONLY the file(s) corresponding to the selected voice
+    flite -voice voices/$voice.flitevox -o $command_subdir/${voice}_in003.wav -t "$command" -s "duration_stretch=1.50"
+
+    find $command_subdir -type f -name ${voice}_in004.wav -exec rm -f {} \; # Clears ONLY the file(s) corresponding to the selected voice
+    flite -voice voices/$voice.flitevox -o $command_subdir/${voice}_in004.wav -t "$command" -s "duration_stretch=1.00" -s "int_f0_target_stddev=50.0"
+
+    find $command_subdir -type f -name ${voice}_in005.wav -exec rm -f {} \; # Clears ONLY the file(s) corresponding to the selected voice
+    flite -voice voices/$voice.flitevox -o $command_subdir/${voice}_in005.wav -t "$command" -s "duration_stretch=1.00" -s "int_f0_target_stddev=75.0"
+
+    find $command_subdir -type f -name ${voice}_in006.wav -exec rm -f {} \; # Clears ONLY the file(s) corresponding to the selected voice
+    flite -voice voices/$voice.flitevox -o $command_subdir/${voice}_in006.wav -t "$command" -s "duration_stretch=1.25" -s "int_f0_target_stddev=50.0"
+
+    find $command_subdir -type f -name ${voice}_in007.wav -exec rm -f {} \; # Clears ONLY the file(s) corresponding to the selected voice
+    flite -voice voices/$voice.flitevox -o $command_subdir/${voice}_in007.wav -t "$command" -s "duration_stretch=1.25" -s "int_f0_target_stddev=75.0"
+
+    find $command_subdir -type f -name ${voice}_in008.wav -exec rm -f {} \; # Clears ONLY the file(s) corresponding to the selected voice
+    flite -voice voices/$voice.flitevox -o $command_subdir/${voice}_in008.wav -t "$command" -s "duration_stretch=1.50" -s "int_f0_target_stddev=50.0"
+
+    find $command_subdir -type f -name ${voice}_in009.wav -exec rm -f {} \; # Clears ONLY the file(s) corresponding to the selected voice
+    flite -voice voices/$voice.flitevox -o $command_subdir/${voice}_in009.wav -t "$command" -s "duration_stretch=1.50" -s "int_f0_target_stddev=75.0"
+
+    set +x
   done
 done
--- record.sh
+++ record.sh
@@ -1,65 +1,94 @@
 #!/bin/bash
 
-trap "{ exit 1; }" SIGINT
+trap "clean_exit" SIGINT
+clean_exit() {
+  echo -e "\nStopping capture and exiting safely\n"
+  sudo pkill -2 tcpdump
+  exit 1
+}
 
-voices=(cmu_us_aew cmu_us_ahw cmu_us_aup cmu_us_awb cmu_us_axb cmu_us_bdl cmu_us_clb cmu_us_eey cmu_us_fem cmu_us_gka cmu_us_jmk cmu_us_ksp cmu_us_ljm cmu_us_rms cmu_us_rxr cmu_us_slt)
+#voices=(cmu_us_aew cmu_us_ahw cmu_us_aup cmu_us_awb cmu_us_axb cmu_us_bdl cmu_us_clb cmu_us_eey cmu_us_fem cmu_us_gka cmu_us_jmk cmu_us_ksp cmu_us_ljm cmu_us_rms cmu_us_rxr cmu_us_slt)
+voices=(cmu_us_aew cmu_us_ahw cmu_us_awb cmu_us_bdl cmu_us_clb cmu_us_fem cmu_us_gka cmu_us_jmk cmu_us_ksp cmu_us_ljm cmu_us_rms cmu_us_rxr cmu_us_slt)
+#voices=(cmu_us_awb)
 
-# Prompt for directory paths
-while read -p "Enter the dataset directory [dataset]: " dataset_dir && dataset_dir=${dataset_dir:-"dataset"} && [ ! -d "$dataset_dir" ]; do
+# Prompt for data directory paths
+while read -p "Enter the dataset directory [current_dataset]: " dataset_dir && dataset_dir=${dataset_dir:-current_dataset} && [ ! -d $dataset_dir ]; do
   echo "Directory doesn't exist"
 done
-echo -e "Using directory \"$dataset_dir\"\n"
-while read -p "Enter the command directory [$dataset_dir/commands]: " command_dir && command_dir=${command_dir:-"$dataset_dir/commands"} && [ ! -d "$command_dir" ]; do
+echo -e "Using directory $dataset_dir\n"
+while read -p "Enter the command directory [$dataset_dir/commands]: " command_dir && command_dir=${command_dir:-$dataset_dir/commands} && [ ! -d $command_dir ]; do
   echo "Directory doesn't exist"
 done
-echo -e "Using directory \"$command_dir\"\n"
-while read -p "Enter the wake word directory [$dataset_dir/wake_words]: " wake_word_dir && wake_word_dir=${wake_word_dir:-"$dataset_dir/wake_words"} && [ ! -d "$wake_word_dir" ]; do
+echo -e "Using directory $command_dir\n"
+while read -p "Enter the wake word directory [$dataset_dir/wake_words]: " wake_word_dir && wake_word_dir=${wake_word_dir:-$dataset_dir/wake_words} && [ ! -d $wake_word_dir ]; do
   echo "Directory doesn't exist"
 done
-echo -e "Using directory \"$wake_word_dir\"\n"
+echo -e "Using directory $wake_word_dir\n"
 
+# Prompt for command subdirectory
 PS3="Select the command subdirectory to use: "
-select command_subdirs in "$command_dir/"* "ALL"; do
+select command_subdirs in $command_dir/* "ALL"; do
   case $command_subdirs in
   ALL)
-    command_subdirs=${command_dir/*/}
-    echo -e "Using ALL directories\n"
+    command_subdirs=$command_dir/*
+    echo -e "Using ALL subdirectories\n"
     break
     ;;
   *)
-    echo -e "Using directory \"$command_subdirs\"\n"
+    echo -e "Using subdirectory $command_subdirs\n"
     break
     ;;
   esac
 done
 
+# Prompt for wake word file
 PS3="Select the wake word file to use: "
-select wake_word_file in "$wake_word_dir/"*; do
+select wake_word_file in $wake_word_dir/*; do
   case $wake_word_file in
   *)
-    echo -e "Using file \"$wake_word_file\"\n"
+    echo -e "Using file $wake_word_file\n"
     break
     ;;
   esac
 done
 
 read -p "Enter the IP address of the device [192.168.1.2]: " ip_addr
-ip_addr=${ip_addr:-"192.168.1.2"}
-echo -e "Using address \"$ip_addr\"\n"
+ip_addr=${ip_addr:-192.168.1.2}
+echo -e "Using address $ip_addr\n"
 
+# For each voice
 for voice in ${voices[@]}; do
-  for command_subdir in $command_subdirs; do
-    rm -f "$command_subdir/"*"out"*
-    i=1
-    for command_file in $command_subdir/${voice}_*.wav; do
-      sudo tcpdump -U -i wlan0 -w $command_subdir/${voice}_out$(printf "%03d" $i).pcap "host $ip_addr" &
+  # For each command subdirectory
+  for command_subdir in ${command_subdirs[@]}; do
+    sudo rm "$command_subdir/${voice}_out"* # Delete stale output files of current voice in current command subdirectory
+    variant=1 # Reset voice variant counter of current voice
+
+    # For each variant file of current voice in current command subdirectory
+    for variant_file in $command_subdir/${voice}_in*; do
+      # Start the capture
+      echo -e "\nCapturing $command_subdir/${voice}_out$(printf '%03d' $variant).pcap\n"
+      sudo tcpdump -U -i wlan0 -w $command_subdir/${voice}_out$(printf "%03d" $variant).pcap "host $ip_addr" &
       paplay $wake_word_file
-      paplay $command_file
-      sox -d $command_subdir/${voice}_out$(printf "%03d" $i).wav silence 1 0.1 5% 1 3.0 5%
+      paplay $variant_file
+
+      # If the capture times out (no response heard after 60 seconds), then redo the capture
+      while ! timeout --foreground 60s sox -d $command_subdir/${voice}_out$(printf "%03d" $variant).wav silence 1 0.1 5% 1 3.0 5%; do
+        # Clean up from failed capture
+        sudo pkill -2 tcpdump
+        sudo rm "$command_subdir/${voice}_out$(printf "%03d" $variant)"*
+
+        # Start the redo capture
+        echo -e "\nCapturing $command_subdir/${voice}_out$(printf '%03d' $variant).pcap\n"
+        sudo tcpdump -U -i wlan0 -w $command_subdir/${voice}_out$(printf "%03d" $variant).pcap "host $ip_addr" &
+        paplay $wake_word_file
+        paplay $variant_file
+      done
+
       sleep 2
       sudo pkill -2 tcpdump
-      ((i++))
+      ((variant++))
     done
-    sudo chown $USER:$USER "$command_subdir/"*
+
+    sudo chown $USER:$USER "$command_subdir/"* # Fix ownership of files
   done
 done
