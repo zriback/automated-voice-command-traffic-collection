fe2e8e07f5909e3090da06d9cc82f6264b2b2604
zriback <zacharythecdr@gmail.com>
2023-03-21 18:18:34 -0400

    Update to sort wav files in directories based on voice used

--- generate.sh
+++ generate.sh
@@ -7,10 +7,10 @@ voices=(cmu_us_awb cmu_us_bdl)
 output_dir="wav_output"
 
 # Prompt for data directory paths
-while read -p "Enter the dataset directory [current_dataset]: " dataset_dir && dataset_dir=${dataset_dir:-current_dataset} && [ ! -d $dataset_dir ]; do
+while read -p "Enter the working directory [.]: " working_dir && working_dir=${working_dir:-.} && [ ! -d $working_dir ]; do
   echo "Directory doesn't exist"
-done; echo -e "Using directory $dataset_dir\n"
-while read -p "Enter the voice_commands file [$dataset_dir/voice_commands]: " voice_commands && voice_commands=${voice_commands:-$dataset_dir/voice_commands} && [ ! -f $voice_commands ]; do
+done; echo -e "Using directory $working_dir\n"
+while read -p "Enter the voice_commands file [$working_dir/voice_commands]: " voice_commands && voice_commands=${voice_commands:-$working_dir/voice_commands} && [ ! -f $voice_commands ]; do
   echo "File does not exist"
 done; echo -e "Using commands file $voice_commands\n"
 
@@ -19,34 +19,35 @@ done; echo -e "Using commands file $voice_commands\n"
 # Adjust the standard deviation of pitch: -s int_f0_target_stddev=25.0
 
 # remove old wav files in $output_dir (if it exists)
-rm -r $output_dir/*.wav 2>/dev/null
-mkdir $dataset_dir/$output_dir 2>/dev/null
+rm -r $working_dir/$output_dir/*.wav 2>/dev/null
+mkdir $working_dir/$output_dir 2>/dev/null
 
 for voice in ${voices[@]}; do
-  cat $dataset_dir/$voice_commands | grep -v "^#" | grep . | while read -r command ; do
+  cat $working_dir/$voice_commands | grep -v "^#" | grep . | while read -r command ; do
     # echo $command
     command_subdir=$(echo $command | sed 's/ /-/g')
-    mkdir $dataset_dir/$output_dir/$command_subdir 2>/dev/null
+    current_dir="$working_dir/$output_dir/$command_subdir/$voice"
+    mkdir -p $current_dir 2>/dev/null
 
     set -x
 
-    flite -voice voices/$voice.flitevox -o $dataset_dir/$output_dir/$command_subdir/${voice}_in001.wav -t "$command" -s "duration_stretch=1.00"
+    flite -voice voices/$voice.flitevox -o $current_dir/var001.wav -t "$command" -s "duration_stretch=1.00"
 
-    flite -voice voices/$voice.flitevox -o $dataset_dir/$output_dir/$command_subdir/${voice}_in002.wav -t "$command" -s "duration_stretch=1.25"
+    flite -voice voices/$voice.flitevox -o $current_dir/var002.wav -t "$command" -s "duration_stretch=1.25"
 
-    flite -voice voices/$voice.flitevox -o $dataset_dir/$output_dir/$command_subdir/${voice}_in003.wav -t "$command" -s "duration_stretch=1.50"
+    flite -voice voices/$voice.flitevox -o $current_dir/var003.wav -t "$command" -s "duration_stretch=1.50"
 
-    flite -voice voices/$voice.flitevox -o $dataset_dir/$output_dir/$command_subdir/${voice}_in004.wav -t "$command" -s "duration_stretch=1.00" -s "int_f0_target_stddev=50.0"
+    flite -voice voices/$voice.flitevox -o $current_dir/var004.wav -t "$command" -s "duration_stretch=1.00" -s "int_f0_target_stddev=50.0"
 
-    flite -voice voices/$voice.flitevox -o $dataset_dir/$output_dir/$command_subdir/${voice}_in005.wav -t "$command" -s "duration_stretch=1.00" -s "int_f0_target_stddev=75.0"
+    flite -voice voices/$voice.flitevox -o $current_dir/var005.wav -t "$command" -s "duration_stretch=1.00" -s "int_f0_target_stddev=75.0"
 
-    flite -voice voices/$voice.flitevox -o $dataset_dir/$output_dir/$command_subdir/${voice}_in006.wav -t "$command" -s "duration_stretch=1.25" -s "int_f0_target_stddev=50.0"
+    flite -voice voices/$voice.flitevox -o $current_dir/var006.wav -t "$command" -s "duration_stretch=1.25" -s "int_f0_target_stddev=50.0"
 
-    flite -voice voices/$voice.flitevox -o $dataset_dir/$output_dir/$command_subdir/${voice}_in007.wav -t "$command" -s "duration_stretch=1.25" -s "int_f0_target_stddev=75.0"
+    flite -voice voices/$voice.flitevox -o $current_dir/var007.wav -t "$command" -s "duration_stretch=1.25" -s "int_f0_target_stddev=75.0"
 
-    flite -voice voices/$voice.flitevox -o $dataset_dir/$output_dir/$command_subdir/${voice}_in008.wav -t "$command" -s "duration_stretch=1.50" -s "int_f0_target_stddev=50.0"
+    flite -voice voices/$voice.flitevox -o $current_dir/var008.wav -t "$command" -s "duration_stretch=1.50" -s "int_f0_target_stddev=50.0"
 
-    flite -voice voices/$voice.flitevox -o $dataset_dir/$output_dir/$command_subdir/${voice}_in009.wav -t "$command" -s "duration_stretch=1.50" -s "int_f0_target_stddev=75.0"
+    flite -voice voices/$voice.flitevox -o $current_dir/var009.wav -t "$command" -s "duration_stretch=1.50" -s "int_f0_target_stddev=75.0"
 
     set +x
   done
