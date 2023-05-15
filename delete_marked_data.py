af6731e63374379318322c568c9dc9da327e71cc
zriback <zacharythecdr@gmail.com>
2023-05-15 18:47:01 -0400

    Add script for assisting in manually deleting bad data

---
+++ delete_marked_data.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+# Script to delete from the data directory bad data as manually marked with a 'd' in filter_data_out.txt
+# format is 
+# d [path to .wav file] [voice assistant output]
+# ^d the d being present, the script will delete that .wav and .pcap file
+
+import os
+
+DEFAULT_FILENAME = 'filter_data_out.txt'
+
+# Get out.txt file
+filename = input(f"Enter output text file name to analyze [{DEFAULT_FILENAME}]: ")
+if filename == "":
+    filename = DEFAULT_FILENAME
+print("Using", filename, "\n")
+
+with open(filename, 'r') as f:
+    for line in f.readlines():
+        fields = line.split()
+        if fields[0] == 'd':  # then delete the file
+            print(f"Removing {os.path.splitext(fields[1])[0]}")
+            try:
+                os.remove(fields[1])
+                os.remove(f"{os.path.splitext(fields[1])[0]}.pcap")
+            except OSError:
+                pass
+
+print("DONE!")
+
