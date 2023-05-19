da2a64ec71a3fbb113d1ef30469ed3089a5b32f8
zriback <zacharythecdr@gmail.com>
2023-05-19 00:10:00 -0400

    Update to take starting directory and capture

--- filter_data.py
+++ filter_data.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+# TODO
+# Now add ability to specify specific capture number that should be touched first in the previously specified directory
+
 import speech_recognition as sr
 import os
 
@@ -12,6 +15,30 @@ def get_input():
         print("Directory does not exist")
     print("Using", data_path, "\n")
 
+    start_dir = input(f"Enter directory in {data_path} to start at (empty=start at beginning): ")
+    while start_dir != "" and not os.path.isdir(f"{data_path}\\{start_dir}"):
+        print("Directory does not exist")
+        start_dir = input(f"Enter directory in {data_path} to start at (empty=start at beginning): ")
+    if not start_dir:
+        print("Starting at beginning of data directory\n")
+    else:
+        print("Using", start_dir, "\n")
+
+    if start_dir:  # if a start_dir was defined, take a start capture, too
+        while True:
+            try:
+                start_num = input(f"Enter capture number to start at (empty=start at beginning): ")
+                if start_num == "":
+                    start_num = '1'
+                if not os.path.isfile(f"{data_path}\\{start_dir}\\cap{start_num}.wav"):
+                    raise ValueError
+                print(f"Starting at cap{start_num}.wav\n")
+                break
+            except ValueError:
+                print(f"cap{start_num}.wav does not exist in")
+    else:
+        start_num = "1"
+        
     # Get value for auto_delete from user
     while True:
         try:
@@ -24,24 +51,27 @@ def get_input():
         except ValueError:
             print("Invalid input - try again (0 or 1)")
 
-    return data_path, auto_delete
+    return data_path, start_dir, start_num, auto_delete
 
 
 # Filter data by checking if .wav file for corresponding capture indicates the voice assistant correctory understood the question
 # automatic_delete_mode set to 1 means it will automatically delete bad pcap files (if set to 0 it will not delete them)
 # put output of text->speech to stdout and txt file
-def filter_data(data, auto_delete):
+def filter_data(data, start_dir, start_num, auto_delete):
     r = sr.Recognizer()
 
-    out_file = open(OUT_FILENAME, "w")
+    # Always open in append mode
+    out_file = open(OUT_FILENAME, "a")
     
-    for question_dir in os.listdir(data):
+    question_dirs = os.listdir(data)
+    if start_dir == "":  # need to set it to the first one
+        start_dir = question_dirs[0]
+    for question_dir in question_dirs[question_dirs.index(start_dir):]:
         print("Analyzing", question_dir)
-        for pcap in os.listdir(os.path.join(data, question_dir)):
-            path = os.path.join(data, question_dir, pcap)
-            if not path.endswith(".wav"):
-                continue
-            
+        pcaps = [i for i in os.listdir(os.path.join(data, question_dir)) if i.endswith('.wav')]
+        for pcap in pcaps[pcaps.index(f"cap{start_num}.wav"):]:
+            path = os.path.join(data, question_dir, pcap) 
+
             try:       
                 with sr.AudioFile(path) as f:
                     audio_data = r.record(f)
@@ -60,9 +90,9 @@ def filter_data(data, auto_delete):
 
 
 def main():
-    data_path, auto_delete = get_input()
+    data_path, start_dir, start_num, auto_delete = get_input()
 
-    filter_data(data_path, auto_delete)
+    filter_data(data_path, start_dir, start_num, auto_delete)
 
 
 if __name__ == '__main__':
